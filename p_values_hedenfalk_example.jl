335b5ef7daba9bdc6f5b7a0e1cf8548277125c07
oskarhs <oskarhsimensen@gmail.com>
2025-04-10 14:58:58 +0200

    Added simulation example.

--- p_values_hedenfalk_example.jl
+++ p_values_hedenfalk_example.jl
@@ -1,9 +1,25 @@
-using AutoHist, Plots
+using AutoHist, Plots, StatsBase
 
-p_values = parse.(Float64, readlines("hedenfalk.txt"))
+function run_example()
+    p_values = parse.(Float64, readlines("hedenfalk.txt"))
 
-H1, _ = histogram_irregular(p_values; grid="regular", support=(0.0, 1.0))
-plot(H1, alpha=0.5)
+    H1, _ = histogram_irregular(p_values; grid="quantile", support=(0.0, 1.0))
+    p1 = plot(H1, alpha=0.5, xlabel="p", ylabel="Density", xlims=[-0.02, 1.02], label="", color="grey", title="Irregular histogram")
 
-H2, _ = histogram_regular(p_values; a = k->0.5*k)
-plot(H2, alpha=0.5)
+    savefig(p1, joinpath("figures", "hedenfalk_irregular.pdf"))
+
+    # Estimate density at p = 1
+    println("Estimated π₀, irregular: $(H1.weights[end])")
+
+    # Count number of observations in leftmost bin
+    println("Number of observations in leftmost bin if irregular histogram $(fit(Histogram, p_values, H1.edges[1]).weights[1])")
+
+
+    H2, _ = histogram_regular(p_values; a = k->0.5*k, support=(0.0, 1.0))
+    p2 = plot(H2, alpha=0.5, xlabel="p", ylabel="Density", xlims=[-0.02, 1.02], label="", color="grey", title="Regular histogram")
+    savefig(p2, joinpath("figures", "hedenfalk_regular.pdf"))
+
+    println("Estimated π₀, regular: $(H2.weights[end])")
+end
+
+run_example()
---
+++ p_values_simulation_example.jl
@@ -0,0 +1,54 @@
+using Random, Distributions, Latexify, AutoHist, DataFrames
+
+function sample_p_vals(rng, π0, n, β)
+    x = Array{Float64}(undef, n)
+    for i = 1:n
+        u = rand(rng, Uniform(0.0, 1.0))
+        if u < π0
+            x[i] = rand(rng, Uniform(0.0, 1.0))
+        else
+            x[i] = rand(rng, Beta(1.0, β))
+        end
+    end
+    return x
+end
+
+function estimate_risks(β)
+    rng = Xoshiro(1812)
+
+    ns = [200, 1000, 5000]
+    π0s = [0.5, 0.8, 0.95]
+
+    B = 500
+    rmse_irr = Array{Float64}(undef, B)
+    rmse_reg = Array{Float64}(undef, B)
+
+    names = ["pi0", "n", "Average RMSE, irregular", "Average RMSE, regular"]
+    risks = DataFrame([[] for _ = names] , names)
+    for j in eachindex(π0s)
+        for i in eachindex(ns)
+            for b = 1:B
+                x = sample_p_vals(rng, π0s[j], ns[i], β)
+                H_irr, _ = histogram_irregular(x; support=(0.0, 1.0), a=1.0, grid="quantile")
+                H_reg, _ = histogram_regular(x; support=(0.0, 1.0), a=k->0.5*k)
+                π0_irr = H_irr.weights[end] # estimated density in last bin
+                π0_reg = H_reg.weights[end]
+                rmse_irr[b] = sqrt(mean((π0s[j] - π0_irr)^2))
+                rmse_reg[b] = sqrt(mean((π0s[j] - π0_reg)^2))
+            end
+            risks = vcat(
+                risks, 
+                DataFrame(
+                    "pi0" => ifelse(i == 1, string(π0s[j]), ""),
+                    "n" => ns[i],
+                    "Average RMSE, irregular" => round(mean(rmse_irr); digits=3),
+                    "Average RMSE, regular" => round(mean(rmse_reg); digits=3)
+                )
+            )
+        end
+    end
+    latexify(risks; env = :table, booktabs = true, latex = false) |> println
+end
+
+#estimate_risks(5.0)
+estimate_risks(10.0)
